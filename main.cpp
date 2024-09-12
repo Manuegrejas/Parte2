07b3faa357880966ab3ce6c5bb79c7194cee6d91
Manuegrejas <142542958+Manuegrejas@users.noreply.github.com>
2024-09-12 21:48:21 +0000

    aula

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Pessoa.cpp"
+#include "MinhasExcecoes.cpp"
 using namespace std;
 
 int main(){
@@ -11,4 +12,36 @@ int main(){
     cout << "CPF: " << p.getCPF() << endl;
     cout << "Data de nascimento: " << p.getNascimento() << endl;
     cout << "Endereco: " << p.getEndereco() << endl;
+
+    int dia, mes, ano;
+
+    while (true){
+    try{
+        cout << "Informe o dia: " << endl;
+        cin >> dia;
+        cout << "Informe o mes: " << endl;
+        cin >> mes;
+        cout << "Informe o ano: " << endl;
+        cin >> ano;
+
+        if(dia<1 || dia>31){
+            throw MinhasExcecoes("Dia Invalido!");
+        } //lancar manualmente uma excessao
+
+        if(mes<1 || mes>12){
+            throw MinhasExcecoes("Mes Invalido!");
+        } 
+
+        if(ano<1999 || ano>2024){
+            throw MinhasExcecoes("Ano Invalido!");
+        } 
+
+        Data d(dia, mes, ano);
+        cout << d.getData() << endl;    
+        break;
+
+    }catch (MinhasExcecoes e){
+        cout << e.mensagem << endl;
+    }
+    }
 }
